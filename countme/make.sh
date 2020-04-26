5f03d8dfa748a2fd8b0531920834379a6f1096db
Arman Ordookhani <arman.ordookhani@gmail.com>
2020-04-27 00:57:30 +0200

    Print metric

--- countme/make.sh
+++ countme/make.sh
@@ -1,10 +1,10 @@
-set -eux
+set -eu
 
 command=$1
 shift;
 
-rate=100
-duration=6
+rate=500
+duration=10
 container_name=countme
 image_name=countme
 
@@ -16,7 +16,7 @@ run-docker)
   container_id=$(docker run -d -p "8080:80" --name $container_name $image_name)
   timeout 60 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/count)" != "200" ]]; do sleep 1; done' || false
   vegeta -cpus 1 attack -rate $rate -duration=${duration}s -targets target.list | vegeta report -type=json | jq '.' > metrics.json
-  cat metrics.json
+  echo $(($(jq '.latencies["99th"]' metrics.json) / 1000000))
   ;;
 *)
   echo Unknown command: $command
