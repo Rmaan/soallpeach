3f63c8b722d27bceb9d4840eccb12f37568575c9
Arman Ordookhani <arman.ordookhani@gmail.com>
2020-04-27 23:38:34 +0200

    Use fasthttp package

--- countme/countme.go
+++ countme/countme.go
@@ -2,53 +2,44 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"github.com/valyala/fasthttp"
 	"log"
-	"net/http"
 	"os"
-	"sync"
+	"runtime"
+	"runtime/pprof"
 	"sync/atomic"
+	"time"
 )
-
-const bufferLength = 32
 
 // Use atomic to update this. It's intentionally a pointer so you don't mistakenly think it's a
 // normal integer.
 var counter = new(int64)
 
-var bufPool = sync.Pool{New: newBuf}
-type bufType [bufferLength]byte
-
-func newBuf() interface{} {
-	return bufType{}
-}
-
-func addHandler(w http.ResponseWriter, r *http.Request) {
-	buf := bufPool.Get().(bufType)
-	defer bufPool.Put(buf)
-
-	n, err := r.Body.Read(buf[:])
-	if err != io.EOF {
-		log.Printf("non EOF from body read: %v", err)
-		w.WriteHeader(500)
+func addHandler(ctx *fasthttp.RequestCtx) {
+	if len(ctx.Path()) > 1 {
+		countHandler(ctx)
 		return
 	}
 
+	buf := ctx.Request.Body()
+	n := len(buf)
 	if n > 0 && buf[n-1] == '\n' {
 		n--
 	}
+
+	// Simple []byte to int
 	count := 0
 	for i := 0; i < n; i++ {
-		count = count * 10 + int(buf[i] - '0')
+		count = count*10 + int(buf[i]-'0')
 	}
 
 	atomic.AddInt64(counter, int64(count))
 }
 
-func countHandler(w http.ResponseWriter, r *http.Request) {
+func countHandler(ctx *fasthttp.RequestCtx) {
 	total := atomic.LoadInt64(counter)
 
-	_, err := fmt.Fprintf(w, "%d", total)
+	_, err := fmt.Fprintf(ctx, "%d", total)
 	if err != nil {
 		log.Printf("err in write %v", err)
 		return
@@ -64,9 +55,27 @@ func envOrDefault(key, def string) string {
 }
 
 func main() {
+	if false {
+		f, err := os.Create("cpu-profile")
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		runtime.SetCPUProfileRate(500)
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+
+		go func() {
+			time.Sleep(time.Second * 15)
+			pprof.StopCPUProfile()
+			f.Close()
+		}()
+	}
+
 	port := envOrDefault("COUNTME_PORT", "8000")
 
-	http.HandleFunc("/count", countHandler)
-	http.HandleFunc("/", addHandler)
-	log.Fatal(http.ListenAndServe(":" + port, nil))
+	if err := fasthttp.ListenAndServe(":"+port, addHandler); err != nil {
+		log.Fatalf("Error in ListenAndServe: %s", err)
+	}
 }
